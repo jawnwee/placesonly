7104a9bf4f00bab81ccf1547c619ae5d74993359
root <jy.johnlee@gmail.com>
2018-07-08 23:23:25 +0000

    index

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>unapp Template</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+	</head>
+	<body>
+	<?php
+		echo "test";	
+	?>
+	</body>
+</html>
+
+
