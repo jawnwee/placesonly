578629bc6e9d267eb06a8f35c2a772b9accc5cc8
John <jy.johnlee@gmail.com>
2018-07-08 19:44:12 -0700

    grid template

--- index.php
+++ index.php
@@ -3,17 +3,34 @@
 	<head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>unapp Template</title>
+	<title>The Accounts</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="" />
 	<meta name="keywords" content="" />
 	<meta name="author" content="" />
+	 <link rel="stylesheet" href="assets/the-accounts.css">
 	</head>
 	<body>
-	<?php
-		echo "test";	
-	?>
+		<div class="grid">
+			<header>
+			    Header
+			  </header>
+
+			  <aside class="sidebar-left">
+			    Left Sidebar
+			  </aside>
+
+			  <article>
+			    Article
+			  </article>
+
+			  <aside class="sidebar-right">
+			    Right Sidebar
+			  </aside>
+			  
+			  <footer>
+			    Footer
+			  </footer>
+		</div>
 	</body>
 </html>
-
-
