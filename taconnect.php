f0653d72a1bc3f057f5309e865d0cd930a5086bf
John <jy.johnlee@gmail.com>
2018-07-14 16:58:30 -0700

    erg

--- taconnect.php
+++ taconnect.php
@@ -15,12 +15,9 @@
 	$result = mysqli_query($conn, $sql);
 	echo $result;
 	if (mysqli_num_rows($result) > 0) {
-	    echo "<table><tr><th>ID</th><th>Name</th></tr>";
-	    // output data of each row
 	    while($row = mysqli_fetch_array($result)) {
-	        echo "<tr><td>".$row["username"]."</td><td>".$row["about"]."</td></tr>";
+	        echo $row;
 	    }
-	    echo "</table>";
 	} else {
 	    echo "0 results";
 	}
