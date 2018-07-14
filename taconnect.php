0eb9cbae2f88c4658549bcfd752e1349c5dc3b2e
John <jy.johnlee@gmail.com>
2018-07-14 16:56:38 -0700

    erg

--- taconnect.php
+++ taconnect.php
@@ -10,18 +10,18 @@
 	echo $conn;
 
 	$sql = "SELECT username, about, url FROM accounts";
-		echo $testvar;
-		echo $conn;
-		$result = mysqli_query($conn, $sql);
-		echo $result;
-		if (mysqli_num_rows($result) > 0) {
-		    echo "<table><tr><th>ID</th><th>Name</th></tr>";
-		    // output data of each row
-		    while($row = mysqli_fetch_array($result)) {
-		        echo "<tr><td>".$row["username"]."</td><td>".$row["about"]."</td></tr>";
-		    }
-		    echo "</table>";
-		} else {
-		    echo "0 results";
-		}
+	echo $testvar;
+	echo $conn;
+	$result = mysqli_query($conn, $sql);
+	echo $result;
+	if (mysqli_num_rows($result) > 0) {
+	    echo "<table><tr><th>ID</th><th>Name</th></tr>";
+	    // output data of each row
+	    while($row = mysqli_fetch_array($result)) {
+	        echo "<tr><td>".$row["username"]."</td><td>".$row["about"]."</td></tr>";
+	    }
+	    echo "</table>";
+	} else {
+	    echo "0 results";
+	}
 ?>
