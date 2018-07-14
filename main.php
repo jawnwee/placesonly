6200f45576e5ca3b82adb4862c8bfb620ea4b706
John <jy.johnlee@gmail.com>
2018-07-14 16:35:12 -0700

    wtf

--- main.php
+++ main.php
@@ -1,14 +1,13 @@
 <?php
 	include './models.php';
-	include './taconnect.php';
+	include 'taconnect.php';
 
 	function getAccounts() {
 		$sql = "SELECT username, about, url FROM accounts";
-		echo "test";
 		echo $conn;
 		echo $testvar;
 		$result = mysqli_query($conn, $sql);
-		echo "test";
+		echo $result;
 		if (mysqli_num_rows($result) > 0) {
 		    echo "<table><tr><th>ID</th><th>Name</th></tr>";
 		    // output data of each row
