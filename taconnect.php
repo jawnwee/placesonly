d631e079d0f2de0639804504d3ad6b441a8f5f29
John <jy.johnlee@gmail.com>
2018-07-14 16:56:15 -0700

    erg

--- taconnect.php
+++ taconnect.php
@@ -7,6 +7,21 @@
 	if ($conn->connect_error) {
     	die("Connection failed: " . $conn->connect_error);
 	}
-	echo "testing sigh";
-	echo $testvar;
+	echo $conn;
+
+	$sql = "SELECT username, about, url FROM accounts";
+		echo $testvar;
+		echo $conn;
+		$result = mysqli_query($conn, $sql);
+		echo $result;
+		if (mysqli_num_rows($result) > 0) {
+		    echo "<table><tr><th>ID</th><th>Name</th></tr>";
+		    // output data of each row
+		    while($row = mysqli_fetch_array($result)) {
+		        echo "<tr><td>".$row["username"]."</td><td>".$row["about"]."</td></tr>";
+		    }
+		    echo "</table>";
+		} else {
+		    echo "0 results";
+		}
 ?>
