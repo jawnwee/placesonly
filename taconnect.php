88a0be20593c76f8a242a03b1161409db4494a30
John <jy.johnlee@gmail.com>
2018-07-13 22:14:10 -0700

    undo old pw

--- taconnect.php
+++ taconnect.php
@@ -1,14 +1,11 @@
- <?php
-$servername = "localhost";
-$username = "root";
-$password = "3961024763cd7cae7708d4cf6e83bea99faa60b921a18d13";
+<?php
+	require_once('../config.php');
+	// Create connection
+	$conn = new mysqli($servername, $username, $password);
 
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected successfully";
+	// Check connection
+	if ($conn->connect_error) {
+    	die("Connection failed: " . $conn->connect_error);
+	} 
+	echo "Connected successfully";
 ?> 
