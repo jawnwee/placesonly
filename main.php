d98391e0fbd2ffc5c2d3f5eb5d95144e104016ea
John <jy.johnlee@gmail.com>
2018-07-14 16:59:55 -0700

    erg

--- main.php
+++ main.php
@@ -3,21 +3,16 @@
 	include 'taconnect.php';
 
 	function getAccounts() {
-		// $sql = "SELECT username, about, url FROM accounts";
-		// echo $testvar;
-		// echo $conn;
-		// $result = mysqli_query($conn, $sql);
-		// echo $result;
-		// if (mysqli_num_rows($result) > 0) {
-		//     echo "<table><tr><th>ID</th><th>Name</th></tr>";
-		//     // output data of each row
-		//     while($row = mysqli_fetch_array($result)) {
-		//         echo "<tr><td>".$row["username"]."</td><td>".$row["about"]."</td></tr>";
-		//     }
-		//     echo "</table>";
-		// } else {
-		//     echo "0 results";
-		// }
+		$sql = "SELECT username, about, url FROM accounts";
+		$result = mysqli_query($conn, $sql);
+		if (mysqli_num_rows($result) > 0) {
+		    while($row = mysqli_fetch_array($result)) {
+		        echo $row;
+		    }
+		    echo "success";
+		} else {
+		    echo "0 results";
+		}
 	}
 
 ?>
--- taconnect.php
+++ taconnect.php
@@ -7,18 +7,4 @@
 	if ($conn->connect_error) {
     	die("Connection failed: " . $conn->connect_error);
 	}
-	// echo $conn;
-
-	$sql = "SELECT username, about, url FROM accounts";
-	echo $testvar;
-	// echo $conn;
-	$result = mysqli_query($conn, $sql);
-	if (mysqli_num_rows($result) > 0) {
-	    while($row = mysqli_fetch_array($result)) {
-	        echo $row;
-	    }
-	    echo "success";
-	} else {
-	    echo "0 results";
-	}
 ?>
