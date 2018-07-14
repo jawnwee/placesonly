30f96e1443b631cb2f96a4771cc8c64ba1655234
John <jy.johnlee@gmail.com>
2018-07-14 14:30:32 -0700

    path

--- main.php
+++ main.php
@@ -1,21 +1,21 @@
 <?php
 	include 'models.php';
-	require_once('taconnect.php');
+	require_once('./taconnect.php');
 
 	function getAccounts() {
-		// $sql = "SELECT username, about, url FROM accounts";
-		// $result = $conn->query($sql);
+		$sql = "SELECT username, about, url FROM accounts";
+		$result = $conn->query($sql);
 
-		// if ($result->num_rows > 0) {
-		//     echo "<table><tr><th>ID</th><th>Name</th></tr>";
-		//     // output data of each row
-		//     while($row = $result->fetch_assoc()) {
-		//         echo "<tr><td>".$row["username"]."</td><td>".$row["about"]."</td></tr>";
-		//     }
-		//     echo "</table>";
-		// } else {
-		//     echo "0 results";
-		// }
+		if ($result->num_rows > 0) {
+		    echo "<table><tr><th>ID</th><th>Name</th></tr>";
+		    // output data of each row
+		    while($row = $result->fetch_assoc()) {
+		        echo "<tr><td>".$row["username"]."</td><td>".$row["about"]."</td></tr>";
+		    }
+		    echo "</table>";
+		} else {
+		    echo "0 results";
+		}
 	}
 
 ?>
