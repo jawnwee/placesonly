4239586499abb3278e4a04df68502c9dd87521d9
John <jy.johnlee@gmail.com>
2018-07-14 17:02:50 -0700

    erg

--- main.php
+++ main.php
@@ -6,7 +6,7 @@
 	$result = mysqli_query($conn, $sql);
 	if (mysqli_num_rows($result) > 0) {
 	    while($row = mysqli_fetch_array($result)) {
-	        echo $row;
+	        echo $row["username"];
 	    }
 	    echo "success";
 	} else {
