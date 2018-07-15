db588856af48f34d7858d67e2b904a4409bd2461
John <jy.johnlee@gmail.com>
2018-07-14 17:03:35 -0700

    erg

--- main.php
+++ main.php
@@ -2,25 +2,23 @@
 	include 'models.php';
 	include 'taconnect.php';
 
-	$sql = "SELECT username, about, url FROM accounts";
-	$result = mysqli_query($conn, $sql);
-	if (mysqli_num_rows($result) > 0) {
-	    while($row = mysqli_fetch_array($result)) {
-	        echo $row["username"];
-	    }
-	    echo "success";
-	} else {
-	    echo "0 results";
-	}
+	// $sql = "SELECT username, about, url FROM accounts";
+	// $result = mysqli_query($conn, $sql);
+	// if (mysqli_num_rows($result) > 0) {
+	//     while($row = mysqli_fetch_array($result)) {
+	//         echo $row["username"];
+	//     }
+	// } else {
+	//     echo "0 results";
+	// }
 
 	function getAccounts() {
 		$sql = "SELECT username, about, url FROM accounts";
 		$result = mysqli_query($conn, $sql);
 		if (mysqli_num_rows($result) > 0) {
 		    while($row = mysqli_fetch_array($result)) {
-		        echo $row;
+	        	echo $row["username"];
 		    }
-		    echo "success";
 		} else {
 		    echo "0 results";
 		}
