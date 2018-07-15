44cb6fe5041bd91ccbbae4ce83ab589ce3bc71c5
John <jy.johnlee@gmail.com>
2018-07-14 17:00:36 -0700

    erg

--- main.php
+++ main.php
@@ -2,6 +2,17 @@
 	include 'models.php';
 	include 'taconnect.php';
 
+	$sql = "SELECT username, about, url FROM accounts";
+	$result = mysqli_query($conn, $sql);
+	if (mysqli_num_rows($result) > 0) {
+	    while($row = mysqli_fetch_array($result)) {
+	        echo $row;
+	    }
+	    echo "success";
+	} else {
+	    echo "0 results";
+	}
+
 	function getAccounts() {
 		$sql = "SELECT username, about, url FROM accounts";
 		$result = mysqli_query($conn, $sql);
