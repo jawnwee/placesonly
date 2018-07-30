7d85174ee3680be01b62caff5f4f1f1196d3f308
John <jy.johnlee@gmail.com>
2018-07-29 17:23:35 -0700

    amend

--- content.php
+++ content.php
@@ -32,15 +32,15 @@
 	<link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
 	<style type="text/css">
 		#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; width:100%;}
-		#mc_embed_signup .button{background-color:#ED4956;}
-				#mc_embed_signup .button:hover{background-color:#ED4956;}
+		#mc_embed_signup .button{background-color:#ED4956; -webkit-appearance: none;}
+				#mc_embed_signup .button:hover{background-color:#ED4956;
+					-webkit-appearance: none;}
 		/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
 		   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
 	</style>
 	<div id="mc_embed_signup">
 	<form action="https://the-accounts.us18.list-manage.com/subscribe/post?u=90ce6f39cf21c630ec1b3176d&amp;id=beec86d5f4" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
 	    <div id="mc_embed_signup_scroll">
-		<h2>Sign up to receive a weekly recap from The Accounts</h2>
 		<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
 	    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
 	    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_90ce6f39cf21c630ec1b3176d_beec86d5f4" tabindex="-1" value=""></div>
