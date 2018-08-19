14af2ae7f0848406b535f047d4e34f1c0eeb4a8a
John <jy.johnlee@gmail.com>
2018-08-19 16:49:58 -0700

    adjust

--- main.php
+++ main.php
@@ -14,13 +14,29 @@
 	<meta name="author" content="" />
 	 <link rel="stylesheet" href="assets/the-accounts.css">
 	</head>
+	<script src='https://api.mapbox.com/mapbox-gl-js/v0.47.0/mapbox-gl.js'></script>
+	<link href='https://api.mapbox.com/mapbox-gl-js/v0.47.0/mapbox-gl.css' rel='stylesheet' />
 	<body>
 		<?php require_once 'header.php'; ?>
 		<div class="grid">
 			<div class="container">
 				<?php require_once 'content.php'; ?>
 			</div>
-		</div>
+			<div id='map' style='width: 400px; height: 300px;'></div>
+				<script>
+				mapboxgl.accessToken = 'pk.eyJ1IjoiamF3bndlZSIsImEiOiJjamwwMTgwYmwxMDV1M3FwNDAweWs3cGV6In0.39iMWxPjecfKgJZIW4zz9g';
+				var map = new mapboxgl.Map({
+					container: 'map',
+				  	// style URL
+					style: 'mapbox://styles/jawnwee/cjl0422b96lls2rparbxgyaqp',
+				  	// initial position in [lon, lat] format
+					center: [ -122.420586,
+        					37.758606],
+				  	// initial zoom
+				 	zoom: 14
+				});
+				</script>
+			</div>
 		<footer>
 			<!-- Footer -->
 		</footer>
