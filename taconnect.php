3268c0ca75f7a3d89b864b92b33d203bae3864eb
John <jy.johnlee@gmail.com>
2018-09-18 22:12:19 -0700

    ta

--- taconnect.php
+++ taconnect.php
@@ -1,7 +1,7 @@
 <?php
 	require_once('../config.php');
 	// Create connection
-	$conn = mysqli_connect($servername, $username, $password, "theaccounts");
+	$conn = mysqli_connect($servername, $username, $password, "placesonly");
 	// Check connection
 	if ($conn->connect_error) {
     	die("Connection failed: " . $conn->connect_error);
